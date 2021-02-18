e1c4e36c39e6267b51fa9d9ac0d1437b4feb6057
Noah Krossa <noahkrossa@gmail.com>
2021-02-18 16:41:15 +0100

    Create test context
    Create this context to not affect database when run tests

--- server.js
+++ server.js
@@ -10,8 +10,16 @@ try {
     console.info(`Express application running in: http://localhost:${PORT}`)
   })
 
+  /** Separate test context to dev/prod contexts
+   * The reason behind this is for not alterate db when run test
+   */
+  const MONGODB_URI =
+    process.env.NODE_ENV === 'development'
+      ? process.env.MONGODB_URI
+      : process.env.MONGODB_TEST_URI
+
   /** Connect to mongodb */
-  connectToMongodb(process.env.MONGODB_URI)
+  connectToMongodb(MONGODB_URI)
 } catch (e) {
   console.log(e)
 }
